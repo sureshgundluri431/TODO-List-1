7ccbb35418a532b726cf3cc98f64abd04b526ec0
sureshgundluri431 <iloveusuresh431@gmail.com>
2024-11-15 09:47:14 +0530

    Database Creation

--- base/views.py
+++ base/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from .models import *
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url=reverse_lazy('tasks')
+
+
+
+
+
+
 
 
