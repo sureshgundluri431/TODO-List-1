3dd6016987bdace478c620812599cc2619a9e03d
sureshgundluri431 <iloveusuresh431@gmail.com>
2024-11-15 23:49:26 +0530

    Login Page creation

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LoginView,LogoutView
 app_name='base'
 
 
 urlpatterns=[
-    
+    path('login/',CustomLogInView.as_view(),name='login'),
+    path('logout/',logout.as_view(),name='logout'),
+    path('',TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('create-task/',TaskCreate.as_view(),name='create-task'),
+    path('update-task/<int:pk>/',TaskUpdate.as_view(),name='update-task'),
+    path('delete-task/<int:pk>/',TaskDelete.as_view(),name='delete-task'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,14 +1,61 @@
-from django.shortcuts import render
+from typing import Set
+from django.shortcuts import render, redirect
 from .models import *
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView,LogoutView
+from django.views import generic
+
+
 # Create your views here.
 
+
+class CustomLogInView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    def get_success_url(self):
+        return reverse_lazy('base:tasks')
+    
+class logout(generic.View):
+    def get(request):
+        logout(request)
+        return redirect('base:login')
+        
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
 
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
-    success_url=reverse_lazy('tasks')
+    success_url = reverse_lazy('base:tasks')
+    template_name = 'base/task_create.html'
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('base:tasks')
+    template_name = 'base/task_create.html'
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task_delete.html'
+    success_url = reverse_lazy('base:tasks')
+
+
+
+    
+
+
+
+
 
 
 
